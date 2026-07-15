d9dc3e2a9a3a0f8dcc5faa35747fea5b92e2561e
neuronemx <rocha.arturo@gmail.com>
2026-07-14 20:59:40 -0600

    Extend active interaction coordinator for Breakout

--- immersa-presentations/active-interaction-coordinator.js
+++ immersa-presentations/active-interaction-coordinator.js
@@ -1,7 +1,8 @@
 class ActiveInteractionCoordinator {
-  constructor({ interactionStore, raffleStore }) {
+  constructor({ interactionStore, raffleStore, breakoutStore = null }) {
     this.interactionStore = interactionStore;
     this.raffleStore = raffleStore;
+    this.breakoutStore = breakoutStore;
     this.locks = new Map();
   }
 
@@ -10,27 +11,4 @@ class ActiveInteractionCoordinator {
     const previous = this.locks.get(key) || Promise.resolve();
     let release;
     const current = new Promise((resolve) => {
-      release = resolve;
-    });
-    const chain = previous.then(() => current);
-    this.locks.set(key, chain);
-
-    await previous;
-    try {
-      return await action();
-    } finally {
-      release();
-      if (this.locks.get(key) === chain) this.locks.delete(key);
-    }
-  }
-
-  hasActiveInteraction(sessionId) {
-    return Boolean(this.interactionStore?.getSession(sessionId)?.active);
-  }
-
-  hasActiveRaffle(sessionId) {
-    return Boolean(this.raffleStore?.getActive(sessionId));
-  }
-}
-
-module.exports = { ActiveInteractionCoordinator };
+      release =
