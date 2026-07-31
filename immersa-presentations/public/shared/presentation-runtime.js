5e647b042885f7a59b509f8d19dfa7d55feca634
neuronemx <rocha.arturo@gmail.com>
2026-07-31 02:33:01 -0600

    Refresh Pong landscape controls

--- immersa-presentations/public/shared/presentation-runtime.js
+++ immersa-presentations/public/shared/presentation-runtime.js
@@ -45,7 +45,7 @@
     if (!root.document.querySelector("link[data-immersa-pong-ui]")) {
       const link = root.document.createElement("link");
       link.rel = "stylesheet";
-      link.href = "/shared/pong-ui.css?v=9";
+      link.href = "/shared/pong-ui.css?v=10";
       link.dataset.immersaPongUi = "1";
       root.document.head.appendChild(link);
     }
