411883afe92756deb74e1dd79fe535fd2ae4f17d
neuronemx <rocha.arturo@gmail.com>
2026-08-27 20:57:40 -0600

    Scope Q&A footprint by Event Hub decks

--- immersa-presentations/event-hub/load-test-footprint.js
+++ immersa-presentations/event-hub/load-test-footprint.js
@@ -42,18 +42,18 @@ class EventHubLoadTestFootprint {
           WHERE execution.event_workspace_id = ?) AS pollResponses,
          (SELECT COUNT(*)
           FROM presentation_sessions presentation
-          INNER JOIN event_live_sessions live ON live.id = presentation.source_session_id
+          INNER JOIN event_live_sessions live ON live.deck_id = presentation.deck_id
           WHERE live.event_workspace_id = ?) AS qnaPresentationSessions,
          (SELECT COUNT(*)
           FROM qna_rounds round
           INNER JOIN presentation_sessions presentation ON presentation.id = round.presentation_session_id
-          INNER JOIN event_live_sessions live ON live.id = presentation.source_session_id
+          INNER JOIN event_live_sessions live ON live.deck_id = presentation.deck_id
           WHERE live.event_workspace_id = ?) AS qnaRounds,
          (SELECT COUNT(*)
           FROM qna_questions question
           INNER JOIN qna_rounds round ON round.id = question.qna_round_id
           INNER JOIN presentation_sessions presentation ON presentation.id = round.presentation_session_id
-          INNER JOIN event_live_sessions live ON live.id = presentation.source_session_id
+          INNER JOIN event_live_sessions live ON live.deck_id = presentation.deck_id
           WHERE live.event_workspace_id = ?) AS qnaQuestions`,
       [
         eventWorkspaceId,
